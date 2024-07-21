624ebc5b579902ca42cdb181e2e9b184d41fbc02
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-07-21 02:08:55 +0000

    Adds fn check_point_bounds to MatrixImage.

--- src/lib.rs
+++ src/lib.rs
@@ -41,21 +41,32 @@ impl MatrixImageBuilder {
 }
 
 impl MatrixImage {
+    /// Checks for bounds within the size of the matrix
+    fn check_point_bounds(&self, point: (u32, u32)) -> Result<bool, Box<dyn Error>> {
+        if point.0 > self.width as u32 || point.1 > self.height as u32 { 
+            return Err(Box::new(error::MatrixError::Overflow)); 
+        } else {
+            Ok(true)
+        }
+    }
     /// Transforms a 2D point reference point into a 1D point correlated with the matrix raw data and its width/height.
     fn to_absolute_point(&self, point: (u32, u32)) -> Result<usize, Box<dyn Error>> {
-        if point.0 > self.width as u32 || point.1 > self.height as u32 { return Err(Box::new(error::MatrixError::Overflow)); };
+        self.check_point_bounds(point)?;
         Ok( (point.0 + point.1 * (self.width as u32)) as usize )
     }
     fn to_2d_point(&self, absolute_point: usize) -> Result<(u32, u32), Box<dyn Error>> {
         let x: u32 = (absolute_point % self.width) as u32;
         let y: u32 = absolute_point as u32 / self.width as u32; 
-        if x > self.width as u32 || y > self.height as u32 { return Err(Box::new(error::MatrixError::Overflow)); };
+        self.check_point_bounds((x,y))?;
         Ok((x,y))
     }
     pub fn edit_point(&mut self, point: (u32, u32), value: u8) -> Result<(), Box<dyn Error>> {
         let absolute_point: usize = self.to_absolute_point(point)?;
         self.data[absolute_point] = value;
         Ok(())
+    }
+    pub fn get_toroidal_neighborhood(&self, point: (u32, u32)) {
+        
     }
     
     pub fn draw(&mut self, color: Channel) -> Result<RgbImage, Box<dyn Error>> {
