f8a71af6d2da2ede87da8bcbd0ae7abd4214d5ac
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-07-23 02:58:04 +0000

    Implements conways_game_of_life.rs in examples.

--- examples/conways_game_of_life.rs
+++ examples/conways_game_of_life.rs
@@ -8,11 +8,12 @@ use std::error::Error;
 use rand::Rng;
 
 const ALIVE_VALUE: u8 = 255;
+const DEAD_VALUE: u8 = 0;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut rng = rand::thread_rng();
-    let n_sequence = 10;
-    let (size_x, size_y) = (10,10);
+    let n_sequence = 100;
+    let (size_x, size_y) = (100,100);
     let mut matrix = MatrixImageBuilder::init().with_height_and_width(size_x,size_y).build();
     
     for point_x in 0..size_x {
@@ -20,7 +21,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             let value: u8 = if rng.gen::<u8>() > 128 {
                 ALIVE_VALUE
             } else {
-                0
+                DEAD_VALUE
             };
             let edit_point = (point_x as u32, point_y as u32);
             let _ = matrix.edit_point(edit_point, value);
@@ -43,20 +44,22 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn still_image(mut matrix: MatrixImage) -> Result<MatrixImage, Box<dyn Error>> {
+    let mut new_matrix = MatrixImageBuilder::init().with_height_and_width(matrix.get_width(),matrix.get_height()).build();
     let size_x = matrix.get_height();
     let size_y = matrix.get_width();
     for point_x in 0..size_x {
         for point_y in 0..size_y {
             let center = (point_x as u32,point_y as u32);
+            let is_alive = matrix.get_point_value(center)? == ALIVE_VALUE;
             let neighborhood = matrix.get_lattice_neighborhood(center, 1, Neighborhood::Moore);
-            let mut sum: u64 = 0;
             let count_hood = count_alive_neighbors(center, neighborhood, &matrix)?;
             #[cfg(debug_assertions)]
             println!("center {center:?} count_hood {count_hood}");
-            let _ = matrix.edit_point(center, ALIVE_VALUE)?;
+            let new_value = conways_ruleset(is_alive, count_hood);
+            let _ = new_matrix.edit_point(center, new_value)?;
         }
     }
-    Ok(matrix)
+    Ok(new_matrix)
 }
 
 fn count_alive_neighbors(center: (u32,u32), neighborhood: Vec<(u32,u32)>, matrix: &MatrixImage) -> Result<u32, Box<dyn Error>> {
@@ -67,4 +70,22 @@ fn count_alive_neighbors(center: (u32,u32), neighborhood: Vec<(u32,u32)>, matrix
     }
     let hood_count = (hood_values.into_iter().map(|x| x as u32).sum::<u32>() - center_value) / ALIVE_VALUE as u32;
     Ok(hood_count)
+}
+
+fn conways_ruleset(is_alive: bool, count: u32) -> u8 {
+    if is_alive {
+        if count < 2 {
+            DEAD_VALUE
+        } else if count > 3 {
+            DEAD_VALUE
+        } else {
+            ALIVE_VALUE
+        }
+    } else {
+        if count == 3 {
+            ALIVE_VALUE
+        } else {
+            DEAD_VALUE
+        }
+    }
 }
