6ba815fd9cbc16aee0b3f3175c63360f187bab2c
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-08-08 02:36:08 +0000

    Prunes example initialization.rs .

--- examples/initialization.rs
+++ examples/initialization.rs
@@ -1,16 +1,12 @@
 use matrix_graph::{
     MatrixImage,
     MatrixImageBuilder,
-    Channel::*,
-    Neighborhood,
 };
 use std::error::Error;
-use rand::Rng;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut rng = rand::thread_rng();
     let (size_x, size_y) = (100,100);
-    let matrix: MatrixImage<u32> = MatrixImageBuilder::init().with_height_and_width(size_x,size_y).build();
+    let _matrix: MatrixImage<u32> = MatrixImageBuilder::init().with_height_and_width(size_x,size_y).build();
     
     Ok(())
 }
