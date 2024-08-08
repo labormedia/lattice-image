3a869fb238023a1d25665d3c4f908e4909a08f56
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-08-08 02:32:59 +0000

    Adds example initialization.rs .

--- examples/initialization.rs
+++ examples/initialization.rs
@@ -10,7 +10,7 @@ use rand::Rng;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut rng = rand::thread_rng();
     let (size_x, size_y) = (100,100);
-    let mut matrix: MatrixImage<u32> = MatrixImageBuilder::init().with_height_and_width(size_x,size_y).build();
+    let matrix: MatrixImage<u32> = MatrixImageBuilder::init().with_height_and_width(size_x,size_y).build();
     
     Ok(())
 }
