47ee0b3c2f45f49a57cbeed9fce66aeac2c5d49c
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-08-12 21:19:49 +0000

    Refactors example element_lattice.

--- examples/element_lattice.rs
+++ examples/element_lattice.rs
@@ -10,19 +10,22 @@ use matrix_graph::{
 use std::error::Error;
 use rand::Rng;
 
+type Point = (u32, u32);
+type MatrixSize = (usize, usize);
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut rng = rand::thread_rng();
-    let (size_x, size_y): (usize, usize) = (100,100);
+    let (size_x, size_y): MatrixSize = (100,100);
     let mut matrix = MatrixImageBuilder::<LatticeElement<f32>>::init().with_height_and_width(size_x,size_y).build();
     
-    for point_x in 0..size_x {
-        for point_y in 0..size_y {
-            let value: LatticeElement<f32> = LatticeElement(rng.gen()); // MatrixImage<LatticeElement<f32>>
-            let _ = matrix.edit_point((point_x as u32, point_y as u32), value);
+    for point_x in 0..(size_x as u32) {
+        for point_y in 0..(size_y as u32) {
+            let value = rng.gen::<f32>(); // LatticeElement<f32>
+            let _ = matrix.edit_point((point_x, point_y), value);
         }
     }
     
-    let center: (u32, u32) = (50,50);
+    let center: Point = (50,50);
     let neighborhood = matrix.get_lattice_neighborhood(center, 3, Neighborhood::VonNeumann);
     
     for point in &neighborhood {
