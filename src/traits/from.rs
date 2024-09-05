43897a0ae247f54fb10a3f0c30e03eb6563bea8e
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-09-05 03:11:10 +0000

    Implements traits From<u8> for LatticeElement<f64>, Max for f64.

--- src/traits/from.rs
+++ src/traits/from.rs
@@ -6,6 +6,12 @@ impl From<LatticeElement<f32>> for f32 {
     }
 }
 
+impl From<LatticeElement<f64>> for f64 {
+    fn from(value: LatticeElement<f64>) -> Self {
+       value.0
+    }
+}
+
 impl From<LatticeElement<u32>> for u32 {
     fn from(value: LatticeElement<u32>) -> Self {
         value.0
@@ -15,6 +21,12 @@ impl From<LatticeElement<u32>> for u32 {
 impl From<LatticeElement<f32>> for u8 {
     fn from(value: LatticeElement<f32>) -> Self {
         ((value.0  / f32::MAX) * (u8::MAX as f32)) as u8
+    }
+}
+
+impl From<LatticeElement<f64>> for u8 {
+    fn from(value: LatticeElement<f64>) -> Self {
+        ((value.0  / f64::MAX) * (u8::MAX as f64)) as u8
     }
 }
 
@@ -39,6 +51,12 @@ impl From<LatticeElement<u8>> for u8 {
 impl From<u8> for LatticeElement<f32> {
     fn from(value: u8) -> Self {
         LatticeElement(value as f32)
+    }
+}
+
+impl From<u8> for LatticeElement<f64> {
+    fn from(value: u8) -> Self {
+        LatticeElement(value as f64)
     }
 }
 
