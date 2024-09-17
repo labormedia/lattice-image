9278c1e7c524a95947234fb814ec8bbdfcee120f
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-09-17 16:29:21 +0000

    Adds fn optimal_objective_with_hood to trait Model.

--- src/traits/model.rs
+++ src/traits/model.rs
@@ -1,10 +1,13 @@
 //!   Describes the structure of a Model, which has an Objective type to optimize,
 //!   an exchange network function to describe the behaviour of the exchange evaluation,
 //!   and and objective function which evaluates two points and returns the type of the Objective.
+
+use crate::traits::Vec;
+
 pub trait Model {
     type Objective;
     type Coefficients;
     fn exchange_network(&mut self, c: Self::Coefficients) -> &mut Self;
     fn optimal_objective(&self, x: (u32, u32)) -> Self::Objective;
-    // TODO: add fn optimal_obective for a custom hood.
+    fn optimal_objective_with_hood(&self, x: (u32, u32), hood: Vec<(u32, u32)>) -> Self::Objective;
 }
