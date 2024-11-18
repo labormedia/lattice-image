618b0f08e4bd93311177c92fa0407574edc3ad69
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-11-18 03:24:14 +0000

    Fixes feature rayon implementation

--- src/hasher/exchange_map.rs
+++ src/hasher/exchange_map.rs
@@ -9,11 +9,42 @@ pub struct ExchangeMap<K, V>  {
     map: BTreeMap<K, V> 
 }
 
+#[cfg(not(feature = "rayon"))]
 impl<K, V> ExchangeMap<K, V> 
 where 
- #[cfg(not(feature = "rayon"))]
  K: PartialOrd + Ord,
- #[cfg(feature = "rayon")]
+{
+    pub fn new() -> Self {
+        ExchangeMap {
+            map: BTreeMap::new()
+        }
+    }
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        self.map.insert(key, value)
+    }
+    pub fn clear(&mut self) {
+        self.map.clear();
+    }
+    pub fn get(&self, key: &K) -> Option<&V> {
+        self.map.get(key)
+    }
+    pub fn iter(&self) -> Iter<'_, K, V> {
+        self.set.iter()
+    }
+    pub fn contains_key(&self, key: &K) -> bool {
+        self.map.contains_key(key)
+    }
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.map.len() == 0
+    }
+}
+
+#[cfg(feature = "rayon")]
+impl<K, V> ExchangeMap<K, V> 
+where 
  K: PartialOrd + Ord + core::marker::Sync, V: core::marker::Sync,
 {
     pub fn new() -> Self {
@@ -31,9 +62,6 @@ where
         self.map.get(key)
     }
     pub fn iter(&self) -> Iter<'_, K, V> {
-        #[cfg(not(feature = "rayon"))]
-        self.set.iter()
-        #[cfg(feature = "rayon")]
         self.set.par_iter()
     }
     pub fn contains_key(&self, key: &K) -> bool {
--- src/hasher/exchange_set.rs
+++ src/hasher/exchange_set.rs
@@ -9,11 +9,42 @@ pub struct ExchangeSet<T> {
     set: BTreeSet<T>
 }
 
+ #[cfg(not(feature = "rayon"))]
 impl<T> ExchangeSet<T> 
 where 
- #[cfg(not(feature = "rayon"))]
  T: PartialOrd + Ord,
- #[cfg(feature = "rayon")]
+{
+    pub fn new() -> Self {
+        ExchangeSet {
+            set: BTreeSet::new()
+        }
+    }
+    pub fn insert(&mut self, value: T) -> bool {
+        self.set.insert(value)
+    }
+    pub fn clear(&mut self) {
+        self.set.clear();
+    }
+    pub fn get(&self, value: &T) -> Option<&T> {
+        self.set.get(value)
+    }
+    pub fn iter(&mut self) -> Iter<'_, T> {
+        self.set.iter()
+    }
+    pub fn contains(&self, value: &T) -> bool {
+        self.set.contains(value)
+    }
+    pub fn len(&self) -> usize {
+        self.set.len()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.set.len() == 0
+    }
+}
+
+#[cfg(feature = "rayon")]
+impl<T> ExchangeSet<T> 
+where 
  T: PartialOrd + Ord + core::marker::Sync,
 {
     pub fn new() -> Self {
@@ -31,9 +62,6 @@ where
         self.set.get(value)
     }
     pub fn iter(&mut self) -> Iter<'_, T> {
-        #[cfg(not(feature = "rayon"))]
-        self.set.iter()
-        #[cfg(feature = "rayon")]
         self.set.par_iter()
     }
     pub fn contains(&self, value: &T) -> bool {
